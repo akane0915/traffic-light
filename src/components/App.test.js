03244d10ec3eec49beb63d83cf510313f2477bd4
akane0915 <asialkane@gmail.com>
2020-04-13 16:34:40 -0700

    add tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,29 +1,59 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { unmountComponentAtNode } from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { unmountComponentAtNode } from 'react-dom';
 
-let container = null;
+import { shallow } from 'enzyme';
 
-beforeEach(() => {
-  container = document.createElement("div");
-  document.body.appendChild(container);
-  ReactDOM.render(<App />, container);
-});
+import { render, fireEvent, screen } from '@testing-library/react';
 
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
+import App from './App';
 
-describe("App", () => {
-  it("renders", () => {
-    expect(container.textContent).toContain(
-      "Click on the traffic light to activate a random light!"
-    );
+describe('App', () => {
+  // describe('ReacDOM', () => {
+  //   it('renders using ReactDOM', () => {
+  //     const container = document.createElement('div');
+  //     document.body.appendChild(container);
+  //     ReactDOM.render(<App />, container);
+
+  //     expect(container.textContent).toContain(
+  //       'Click on the traffic light to activate a random light!'
+  //     );
+  //   });
+  // });
+
+  // describe('Enzyme', () => {
+  //   let appComponent;
+
+  //   beforeEach(() => {
+  //     appComponent = shallow(<App />);
+  //   });
+
+  //   it('renders using enzyme', () => {
+  //     expect(appComponent.contains('Click on the traffic light to activate a random light!')).toEqual(true);
+  //   });
+
+  //   describe('lights', () => {
+  //     it('does not have an "active" classname to start', () => {
+  //       const topLight = appComponent.find('.top-light');
+  //       expect(topLight.hasClass('active')).toEqual(false);
+  //     });
+  //   });
+  // });
+
+  describe('React Testing Library', () => {
+    let myApp;
+
+    beforeEach(() => {
+      myApp = render(<App />);
+    });
+
+    it('renders using react testing library', () => {
+      const { getByText } = myApp;
+      expect(getByText('Click on the traffic light to activate a random light!')).toBeInTheDocument();
+    });
+
+    it('activates a light upon clicking traffic light', () => {
+      fireEvent.click(screen.getByRole('button'));
+    });
   });
-
-  // I would normally add significantly more unit tests, but wanted to respect
-  // the requested time constraint.
 });
